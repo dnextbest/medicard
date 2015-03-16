8d508d95e944c910aa3d4667f9459d8f9f3bae1e
dnextbest <dominosz89@gmail.com>
2015-03-16 23:53:34 +0100

    Initial commit

---
+++ client/views/doctor/patientsList/patientsList.js
@@ -0,0 +1,18 @@
+Template.patientsList.patients = [
+  {
+    ID: 'blabla',
+    name: 'Asia',
+    surname: 'Wsciekla',
+    address: 'Rolnicza',
+    city: 'wies',
+    code: '123412'
+  },
+  {
+    ID: 'blabla2',
+    name: 'Asia2',
+    surname: 'Wsciekla2',
+    address: 'Rolnicza2',
+    city: 'wies2',
+    code: '1234121'
+  },
+];
---
+++ client/views/patient/patientData/patientData.js
@@ -0,0 +1,1 @@
+Template.patientData.patient = this;
